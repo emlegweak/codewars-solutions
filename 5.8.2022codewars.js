0c65e1a497bd74c823e1cd6398ac68bd28f39c6a
emlegweak <emilythearmstrong@gmail.com>
2022-05-08 17:07:46 -0400

    5.8.2022 codewars solution

---
+++ 5.8.2022codewars.js
@@ -0,0 +1,11 @@
+//Multiple of Index
+
+function multipleOfIndex(array) {
+    let newArray = [];
+    for (let i = 0; i <= array.length; i++) {
+        if (array[i] % i === 0) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
