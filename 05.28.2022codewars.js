53bcf11d781956e1f3ff3bb111d99356d7708301
emlegweak <emilythearmstrong@gmail.com>
2022-05-28 11:59:00 +0000

    5.28.2022 codewars solution

--- 05.28.2022codewars.js
+++ 05.28.2022codewars.js
@@ -6,4 +6,4 @@ function likes(names) {
     if (names.length === 2) return `${names[0]} and ${names[1]} like this`
     if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`
     if (names.length > 3) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
-}
+} 
