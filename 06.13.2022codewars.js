ae506f126693b572006d4e446a9b4f819a04a7aa
emlegweak <emilythearmstrong@gmail.com>
2022-06-13 11:59:00 +0000

    codewars solution for 06.13.2022

---
+++ 06.13.2022codewars.js
@@ -0,0 +1,14 @@
+//What's up next?
+function nextItem(xs, item) {
+    let found = false;
+    let finalValue
+
+    for (let i of xs) {
+        if (found) {
+            finalValue = i;
+            break;
+        }
+        if (i === item) found = true
+    }
+    return finalValue
+}
