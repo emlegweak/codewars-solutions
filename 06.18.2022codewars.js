cdccbe4023b5360f28ed40ddee70f51f2d90779a
emlegweak <emilythearmstrong@gmail.com>
2022-06-18 22:29:27 -0400

    codewars solution for 06.18.2022

---
+++ 06.18.2022codewars.js
@@ -0,0 +1,10 @@
+//object-oriented piracy
+class Ship {
+  constructor(draft, crew){
+    this.draft = draft;
+    this.crew = crew;
+  }
+  isWorthIt(){
+    return this.draft-1.5 * this.crew > 20
+  }
+}
