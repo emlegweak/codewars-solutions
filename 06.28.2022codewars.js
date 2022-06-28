35a32f232549f0fd2b4699621b0e827c870cda0b
emlegweak <emilythearmstrong@gmail.com>
2022-06-28 11:59:00 +0000

    codewars solution for 06.28.2022

---
+++ 06.28.2022codewars.js
@@ -0,0 +1,13 @@
+function longest(s1, s2) {
+    // your code
+    let a = [... new Set(s1.split(""))];
+    let b = [... new Set(s2.split(""))];
+
+    for (let i = 0; i < b.length; i++) {
+        a.push(b[i]);
+    }
+    let init_combo = [... new Set(a)];
+    let combo = init_combo.sort().join("");
+    return combo;
+
+}
