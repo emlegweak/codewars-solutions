1fc09ce47a57ab928e4b26255824ac9becd0193e
emlegweak <emilythearmstrong@gmail.com>
2022-08-04 11:59:00 +0000

    codewars solution for 08.04.2022

---
+++ 08.04.2022codewars.js
@@ -0,0 +1,10 @@
+//Grasshopper - Terminal Game #1
+class Hero {
+    constructor(name = 'Hero', position, health, damage, experience) {
+        this.name = name
+        this.position = '00'
+        this.health = 100
+        this.damage = 5
+        this.experience = 0
+    }
+}
