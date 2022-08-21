415f8ab3ebe4cc6c4f2945fde21400fc69a78001
emlegweak <emilythearmstrong@gmail.com>
2022-08-21 09:48:15 -0400

    codewars solution for 08.21.2022

---
+++ 08.21.2022codewars.js
@@ -0,0 +1,17 @@
+//Fun with ES6 Classes #1 - People, people, people
+class Person {
+    constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.gender = gender
+    }
+    sayFullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+Person.greetExtraTerrestrials = function (raceName) {
+    return `Welcome to Planet Earth ${raceName}`
+}
+
