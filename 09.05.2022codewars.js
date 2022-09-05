9a95423ff6d352552eafacc2f3cbfd7ec4ae20a4
emlegweak <emilythearmstrong@gmail.com>
2022-09-05 11:23:00 +0000

    more codewars and more string methods

---
+++ 09.05.2022codewars.js
@@ -0,0 +1,5 @@
+//convert an array of strings to array of numbers
+//ya girl JUST read about parsefloat so like, this is legit amazing
+function toNumberArray(stringarray) {
+    return stringarray.map(parseFloat)
+}
