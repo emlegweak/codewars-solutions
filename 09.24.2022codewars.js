8cba2901b5b64642a5098904b17b2d46802a7b90
emlegweak <emilythearmstrong@gmail.com>
2022-09-25 11:59:00 +0000

    codewars solution

---
+++ 09.24.2022codewars.js
@@ -0,0 +1,17 @@
+//are the numbers in order?
+
+function inAscOrder(arr) {
+  // Code your algorithm here :)
+  
+  // Hmmm ... maybe we should try our luck out ...
+  // if (Math.random() > 0.5) {
+  //  return true;
+  // } else {
+  //  return false;
+  // }
+  
+ for(let i = 0; i < arr.length - 1; i++){
+  if(arr[i] > arr[i + 1]) return false 
+ }
+  return true
+}
