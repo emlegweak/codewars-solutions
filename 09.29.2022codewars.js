852fc40b7068bd1c000565ca22405d9fb6e368d1
emlegweak <emilythearmstrong@gmail.com>
2022-09-29 10:00:50 -0400

    codewars solution

---
+++ 09.29.2022codewars.js
@@ -0,0 +1,10 @@
+//find the divisors
+function divisors(integer) {
+  let result = []
+  for(let i = 2; i < integer; i++){
+    if(integer % i === 0){
+      result.push(i)
+    }
+  }
+  return result.length !== 0 ? result : `${integer} is prime`
+};
