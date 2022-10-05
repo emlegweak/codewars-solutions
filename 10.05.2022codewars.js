825fb58aad2d6cb96b9d82954df3618c0ffa706b
emlegweak <emilythearmstrong@gmail.com>
2022-10-05 11:59:00 +0000

    codewars solution

---
+++ 10.05.2022codewars.js
@@ -0,0 +1,11 @@
+//power of 2
+function isPowerOfTwo(n){
+  if(n == 0) return false
+  while(n !== 1){
+    n = n/2
+    if(n % 2 !== 0 && n !== 1){
+      return false
+    }
+  }
+  return true
+}
