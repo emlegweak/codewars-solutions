a2cd9499fa6b564e16617ec84d74bb79d525f606
emlegweak <emilythearmstrong@gmail.com>
2022-11-21 15:11:09 -0500

    back in the saddle, baby!

---
+++ 11.21.2022codewars.js
@@ -0,0 +1,8 @@
+
+//Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+//"alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+//--> "alpha beta gamma delta alpha beta gamma delta"
+
+const removeConsecutiveDuplicates = s => s.split(' ').filter((val, i, arr) => val != arr[i + 1]).join(' ')
